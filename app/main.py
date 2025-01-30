9faafa9f0ae483cc076bf57673b240b71e7c2347
devcodes001 <devnarayanpunthala3@gmail.com>
2025-01-30 15:45:47 +0530

    love

--- app/main.py
+++ app/main.py
@@ -29,7 +29,7 @@ def main():
             My love, these two years have been the most precious of my life. Through every challenge, our love remained unshaken, and I’m so grateful I chose you. You’ve helped me grow, made me stronger, and filled my world with happiness. I’ll always cherish the love we share. Happy Birthday, my love! May I be blessed with a lifetime of moments to celebrate you. I love you endlessly, today and always, with all my heart. ❤️
             """
             st.write(intro_text)
-            audio_file = open("rec.mp3", "rb")
+            audio_file = open("img/rec.mp3", "rb")
             audio_bytes = audio_file.read()
             st.audio(audio_bytes, format="audio/mpeg")
     with tab3:
